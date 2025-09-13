722d3d15c66d62ae7b1cc649d85760f1c7aaa24f
Jakub Janák <jakub.janak@icloud.com>
2025-09-13 15:44:15 +0200

    Implementace react-router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter } from "react-router"
+import { RouterProvider } from "react-router/dom"
+import "./index.css"
+import Homepage from "./Homepage"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />
+  },
+]);
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
 )
